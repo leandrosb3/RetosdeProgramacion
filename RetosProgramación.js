d9328b2ee8457cabc0feaa8d0b98b4c7d257d4ee
Leandro Santiago <leandrosb_03@hotmail.com>
2020-06-08 14:29:06 -0400

    Solucion reto 2

--- RetosProgramación.js
+++ RetosProgramación.js
@@ -1,3 +1,8 @@
 //Reto #1 “Hola Mundo”
 var nombre = prompt("Cual es tu nombre");
 alert('Hola '+ nombre);
+
+//Reto #2 “Hola… nombre y apellido:”
+var nombre = prompt("Cual es tu nombre");
+var apellido = prompt("CUal es tu apellido");
+alert('Hola '+ nombre +' '+ apellido);
