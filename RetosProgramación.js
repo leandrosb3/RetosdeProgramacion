191438127e02162b1bf49a2773fa0ec333d4daf2
Leandro Santiago <leandrosb_03@hotmail.com>
2020-06-08 14:30:47 -0400

    Se soluciona el reto 3, 4 y 5

--- RetosProgramación.js
+++ RetosProgramación.js
@@ -6,3 +6,39 @@ alert('Hola '+ nombre);
 var nombre = prompt("Cual es tu nombre");
 var apellido = prompt("CUal es tu apellido");
 alert('Hola '+ nombre +' '+ apellido);
+
+//Reto #3 “Mensaje multilínea”
+console.log('Platzi cuenta con cursos de: ');
+for(var x = 1 ; x < 7; x++)
+{
+    console.log('Categoria'+x);
+}
+
+//Reto #4 “Suma de enteros”
+var valor1 = prompt('inserta el primer numero'); 
+var valor2 = prompt('inserta el segundo numero');
+
+functionSuma(valor1, valor2, resultado)
+{ 
+    
+    resultado = parseFloat(valor1) + parseFloat(valor2);
+    return alert( valor1 + '+' + valor2 +  '=' + resultado );
+}
+
+var sumar = Suma(valor1, valor2);
+
+//Reto #5 “Suma y multiplicación”
+var valor1 = prompt('inserta el primer numero'); 
+var valor2 = prompt('inserta el segundo numero');
+var valor3 = prompt('inserta el multiplo');
+
+functionSuma(valor1, valor2, valor3, resultado)
+{ 
+    
+    resultado = parseFloat(valor1) + parseFloat(valor2);
+    resultado = resultado * parseFloat(valor3);
+    return alert('Datos de entrada: '+valor1+ ', ' +valor2+ ', ' +valor3 );
+
+}
+
+var sumar = Suma(valor1, valor2, valor3);
