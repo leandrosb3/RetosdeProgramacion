590eec372e2c43b5c34d04414a34a01bd86b8977
Leandro <34948075+leandrosb3@users.noreply.github.com>
2020-06-08 14:31:52 -0400

    Solucion retos
    Se solucionan los retos 5, 6 y 7

--- RetosProgramación.js
+++ RetosProgramación.js
@@ -42,3 +42,34 @@ functionSuma(valor1, valor2, valor3, resultado)
 }
 
 var sumar = Suma(valor1, valor2, valor3);
+
+//Reto #6 “Resta de pizzas”
+
+var rebanadas = prompt('Cuantas rebanadas de pizza hay disponibles? ');
+var Consumida = prompt('Cuantas rebanadas se han consumido? ');
+
+functionrebanadasRestantes(x, y, z)
+{
+    z = parseInt(x) - parseInt(y);
+   
+    if(z == 0){
+    return alert('Ya no quedan rebanadas disponibles');
+    } elseif ( z == 1){
+    return alert('Solo queda una rebanada de pizza disponible');
+    } elseif (z < 0 ){
+        return alert ('Entonces me cuentas que se ha consumido mas rebanadas que las que habian disponibles jajajaja');
+    } elseif (z > 1 ){
+        return alert ('Quedan '+z+ ' rebanadas disponibles ');
+    }
+  
+}
+
+var resultado = rebanadasRestantes(rebanadas, Consumida);
+
+//Reto #7 “Edad futura y pasada”
+var nombre = prompt("Cual es tu nombre? ");
+var edad = prompt('Cual es tu edad? ');
+var ePasada = edad - 1;
+var efutura = parseInt(edad) + parseInt(1);
+
+alert(nombre + ' el año pasado tenias ' + ePasada+ ' años y el año proximo tendras ' + efutura +' años' )
